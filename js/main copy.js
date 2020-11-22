72b98690c597d7d69221284a1e23a5c02b3cf176
William <w.watson@uqconnect.edu.au>
2020-11-23 09:41:36 +1000

    Patch0.1

---
+++ js/main copy.js
@@ -0,0 +1,114 @@
+$( document ).ready(function() {
+   makeCharacter();
+});
+
+function makeCharacter() {
+ 
+    var size = 24;
+
+    $.getJSON("data.json", function (data) {
+        $.each(data, function (index, value) {
+            switch (index) {
+                case "Characters":
+                    if (randNum(0,1)) {
+                        document.getElementById("first-name").innerHTML = value.FemaleNames[randNum(0,Object.keys(value.FemaleNames).length-1)];
+                    } else {
+                        document.getElementById("first-name").innerHTML = value.MaleNames[randNum(0,Object.keys(value.MaleNames).length-1)];
+                    }
+                    if (randNum(0,1)) {
+                        document.getElementById("last-name").innerHTML = value.UpperClassSurnames[randNum(0,Object.keys(value.UpperClassSurnames).length-1)];
+                    } else {
+                        document.getElementById("last-name").innerHTML = value.LowerClassSurnames[randNum(0,Object.keys(value.LowerClassSurnames).length-1)];
+                    }
+                    break;
+                /*case "CivilizedJobs":
+                    document.getElementById("job").innerHTML = value[randNum(0,size-1)];
+                    break;*/
+                case "Assets":
+                    document.getElementById("assets").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;
+                case "Liabilities":
+                    document.getElementById("liabilities").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;  
+                case "Goals":
+                    var selection = randNum(0, Object.keys(value).length-1);
+                    if (selection == 2) {
+                        document.getElementById("goals").innerHTML = "Aquire " + data.Items[randNum(0, Object.keys(value).length - 1)];
+                    } else if (selection == 3) {
+                        document.getElementById("goals").innerHTML = "Craft " + data.Items[randNum(0, Object.keys(value).length - 1)];
+                    } else if (selection == 5) {
+                        document.getElementById("goals").innerHTML = "Destroy " + data.Items[randNum(0, Object.keys(value).length - 1)];
+                    }
+                    else {
+                        document.getElementById("goals").innerHTML = value[selection];
+                    }
+                    break;
+                case "Misfortunes":
+                    document.getElementById("misfortunes").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;
+                case "Missions":
+                    document.getElementById("missions").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;
+                case "Appearances":
+                    document.getElementById("appearance").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;
+                case "PhysicalDetails":
+                    document.getElementById("details").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;
+                case "Clothing":
+                    document.getElementById("clothing").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;
+                case "Personalities":
+                    document.getElementById("personality").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;
+                case "Mannerisms":
+                    document.getElementById("mannerism").innerHTML = value[randNum(0,Object.keys(value).length-1)];
+                    break;
+                case "Secrets":
+                    var selection = randNum(0, 1);
+                    if (selection) {
+                        document.getElementById("secrets").innerHTML = data.Misfortunes[randNum(0, size - 1)];
+                    } else {
+                        document.getElementById("secrets").innerHTML = value[selection];
+                    }
+                    break;
+                case "Reputations":
+                    document.getElementById("reputations").innerHTML = value[randNum(0,size-1)];
+                    break;
+                case "Relationships":
+                    document.getElementById("relationships").innerHTML = value[randNum(0,size-1)];
+                    break;
+                case "Methods":
+                    document.getElementById("methods").innerHTML = value[randNum(0,size-1)];
+                    break;
+                case "Factions":
+                    var selection = randNum(0,size-1);
+                    if (selection == 21) {
+                        document.getElementById("faction-type").innerHTML = data.Personalities[randNum(0, size-1)];
+                    } else {
+                       document.getElementById("faction-type").innerHTML = value[selection]; 
+                    }
+                    break;
+                case "FactionTraits":
+                    document.getElementById("faction-traits").innerHTML = value[randNum(0,size-1)];
+                    break;
+                case "FactionGoals":
+                    document.getElementById("faction-goals").innerHTML = value[randNum(0,size-1)];
+                    break;
+            }
+         });
+    });
+}
+
+function randNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function hideMore() {
+    var x = document.getElementById("more");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,107 +1,135 @@
+
 $( document ).ready(function() {
-   makeCharacter();
+    count = randNum(1,4);
+    for(var i=0; i < count; i++){
+        house = makeHouse(i);
+        peopleCount = randNum(1,5);
+        for(var j=0; j < peopleCount; j++) {
+            newChar(i, j);
+            makeCharacter(i, j);
+        }
+       
+    }
 });
 
-function makeCharacter() {
- 
-    var size = 24;
-
+function makeCharacter(houseid, id) {
     $.getJSON("data.json", function (data) {
         $.each(data, function (index, value) {
             switch (index) {
                 case "Characters":
                     if (randNum(0,1)) {
-                        document.getElementById("first-name").innerHTML = value.FemaleNames[randNum(0,Object.keys(value.FemaleNames).length-1)];
+                        document.getElementById("first-name" + houseid + id).innerHTML = value.FemaleNames[randNum(0,Object.keys(value.FemaleNames).length-1)];
                     } else {
-                        document.getElementById("first-name").innerHTML = value.MaleNames[randNum(0,Object.keys(value.MaleNames).length-1)];
+                        document.getElementById("first-name" + houseid + id).innerHTML = value.MaleNames[randNum(0,Object.keys(value.MaleNames).length-1)];
                     }
                     if (randNum(0,1)) {
-                        document.getElementById("last-name").innerHTML = value.UpperClassSurnames[randNum(0,Object.keys(value.UpperClassSurnames).length-1)];
+                        document.getElementById("last-name" + houseid + id).innerHTML = value.UpperClassSurnames[randNum(0,Object.keys(value.UpperClassSurnames).length-1)];
                     } else {
-                        document.getElementById("last-name").innerHTML = value.LowerClassSurnames[randNum(0,Object.keys(value.LowerClassSurnames).length-1)];
+                        document.getElementById("last-name" + houseid + id).innerHTML = value.LowerClassSurnames[randNum(0,Object.keys(value.LowerClassSurnames).length-1)];
                     }
                     break;
                 /*case "CivilizedJobs":
                     document.getElementById("job").innerHTML = value[randNum(0,size-1)];
                     break;*/
-                case "Assets":
-                    document.getElementById("assets").innerHTML = value[randNum(0,Object.keys(value).length-1)];
-                    break;
-                case "Liabilities":
-                    document.getElementById("liabilities").innerHTML = value[randNum(0,Object.keys(value).length-1)];
-                    break;  
                 case "Goals":
                     var selection = randNum(0, Object.keys(value).length-1);
                     if (selection == 2) {
-                        document.getElementById("goals").innerHTML = "Aquire " + data.Items[randNum(0, Object.keys(value).length - 1)];
+                        document.getElementById("goals" + houseid + id).innerHTML = "Aquire " + data.Items[randNum(0, Object.keys(value).length - 1)];
                     } else if (selection == 3) {
-                        document.getElementById("goals").innerHTML = "Craft " + data.Items[randNum(0, Object.keys(value).length - 1)];
+                        document.getElementById("goals" + houseid + id).innerHTML = "Craft " + data.Items[randNum(0, Object.keys(value).length - 1)];
                     } else if (selection == 5) {
-                        document.getElementById("goals").innerHTML = "Destroy " + data.Items[randNum(0, Object.keys(value).length - 1)];
+                        document.getElementById("goals" + houseid + id).innerHTML = "Destroy " + data.Items[randNum(0, Object.keys(value).length - 1)];
                     }
                     else {
-                        document.getElementById("goals").innerHTML = value[selection];
+                        document.getElementById("goals" + houseid + id).innerHTML = value[selection];
                     }
-                    break;
-                case "Misfortunes":
-                    document.getElementById("misfortunes").innerHTML = value[randNum(0,Object.keys(value).length-1)];
-                    break;
-                case "Missions":
-                    document.getElementById("missions").innerHTML = value[randNum(0,Object.keys(value).length-1)];
-                    break;
-                case "Appearances":
-                    document.getElementById("appearance").innerHTML = value[randNum(0,Object.keys(value).length-1)];
-                    break;
-                case "PhysicalDetails":
-                    document.getElementById("details").innerHTML = value[randNum(0,Object.keys(value).length-1)];
-                    break;
-                case "Clothing":
-                    document.getElementById("clothing").innerHTML = value[randNum(0,Object.keys(value).length-1)];
                     break;
                 case "Personalities":
-                    document.getElementById("personality").innerHTML = value[randNum(0,Object.keys(value).length-1)];
-                    break;
-                case "Mannerisms":
-                    document.getElementById("mannerism").innerHTML = value[randNum(0,Object.keys(value).length-1)];
-                    break;
-                case "Secrets":
-                    var selection = randNum(0, 1);
-                    if (selection) {
-                        document.getElementById("secrets").innerHTML = data.Misfortunes[randNum(0, size - 1)];
-                    } else {
-                        document.getElementById("secrets").innerHTML = value[selection];
-                    }
-                    break;
-                case "Reputations":
-                    document.getElementById("reputations").innerHTML = value[randNum(0,size-1)];
-                    break;
-                case "Relationships":
-                    document.getElementById("relationships").innerHTML = value[randNum(0,size-1)];
-                    break;
-                case "Methods":
-                    document.getElementById("methods").innerHTML = value[randNum(0,size-1)];
-                    break;
-                case "Factions":
-                    var selection = randNum(0,size-1);
-                    if (selection == 21) {
-                        document.getElementById("faction-type").innerHTML = data.Personalities[randNum(0, size-1)];
-                    } else {
-                       document.getElementById("faction-type").innerHTML = value[selection]; 
-                    }
-                    break;
-                case "FactionTraits":
-                    document.getElementById("faction-traits").innerHTML = value[randNum(0,size-1)];
-                    break;
-                case "FactionGoals":
-                    document.getElementById("faction-goals").innerHTML = value[randNum(0,size-1)];
+                    document.getElementById("personality" + houseid + id).innerHTML = value[randNum(0,Object.keys(value).length-1)];
                     break;
             }
          });
     });
 }
 
+function makeHouse(id) {
+    var main = document.getElementById("main");
+
+    var houseTitleBox = document.createElement("div");
+    houseTitleBox.setAttribute("class", "row");
+    var houseTitle = document.createElement("h1");
+    var houseText = document.createTextNode("House " + id);
+    houseTitle.setAttribute("class", "mw-100");
+    houseTitle.appendChild(houseText);
+    houseTitleBox.appendChild(houseTitle);
+    main.appendChild(houseTitleBox);
+
+    var houseToggle = document.createElement("a");
+    houseToggle.setAttribute("href", "#house" + id);
+    houseToggle.setAttribute("class", "btn btn-info");
+    houseToggle.setAttribute("data-toggle", "collapse");
+    var houseToggletext = document.createTextNode("Toggle");
+    houseToggle.appendChild(houseToggletext);
+    main.appendChild(houseToggle);
+
+    var house = document.createElement("div");
+    house.setAttribute("class", "row");
+    house.setAttribute("id", "house" + id);
+    house.setAttribute("class", "collapse");
+    main.appendChild(house);
+
+    return house;
+}
+
+function newChar(houseid, id) {
+    var outer = document.createElement("div");
+    outer.setAttribute("class", "col-md");
+    house.appendChild(outer);
+    
+    var heading = document.createElement("div");
+    heading.setAttribute("class", "heading");
+    outer.appendChild(heading);
+
+    var firstname = document.createElement("h1");
+    firstname.setAttribute("id", "first-name" + houseid + id);
+    var lastname = document.createElement("h1");
+    lastname.setAttribute("id", "last-name" + houseid + id);
+    heading.appendChild(firstname);
+    heading.appendChild(lastname);
+
+    var dl = document.createElement("dl");
+    var keyinfo = document.createElement("div");
+    keyinfo.setAttribute("class", "subtitle");
+    var keytext = document.createTextNode("Key Information");
+    keyinfo.appendChild(keytext);
+    dl.appendChild(keyinfo);
+    outer.appendChild(dl);
+
+    var dtpersonality = document.createElement("dt");
+    var personalitytext = document.createTextNode("Personality");
+    dtpersonality.appendChild(personalitytext);
+    dl.appendChild(dtpersonality)
+    var ddpersonality = document.createElement("dd");
+    ddpersonality.setAttribute("id", "personality" + houseid + id);
+    dl.appendChild(ddpersonality);
+
+    var dtgoal = document.createElement("dt");
+    var personalitytext = document.createTextNode("Goal");
+    dtgoal.appendChild(personalitytext);
+    dl.appendChild(dtgoal)
+    var ddgoal = document.createElement("dd");
+    ddgoal.setAttribute("id", "goals" + houseid + id);
+    dl.appendChild(ddgoal);
+}
+
 function randNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function updateAll() {
+    for(var i=0; i < count; i++){
+        makeCharacter(i);
+    }
 }
 
 function hideMore() {
